a0171fe65f394e2cbdf4f52fbbfdbf9cbe5f27b8
Avishkar <singhavishkar177@gmail.com>
2023-06-26 11:37:26 +0530

    example chan22ge22

--- src/main/java/com/example/Employeemanagementsystem/repository/EmpRepository.java
+++ src/main/java/com/example/Employeemanagementsystem/repository/EmpRepository.java
@@ -4,5 +4,5 @@ import com.example.Employeemanagementsystem.Model.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface EmpRepository extends JpaRepository<Employee,Integer> {
-
+//new code
 }
