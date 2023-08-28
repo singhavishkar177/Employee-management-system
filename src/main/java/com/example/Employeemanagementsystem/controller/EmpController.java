a8a13d826bdcce9c7546a7f60f89e996788a48c7
Avishkar <singhavishkar177@gmail.com>
2023-08-28 14:02:59 +0530

    added spring openapi docs swagger api

--- src/main/java/com/example/Employeemanagementsystem/controller/EmpController.java
+++ src/main/java/com/example/Employeemanagementsystem/controller/EmpController.java
@@ -4,6 +4,10 @@ import com.example.Employeemanagementsystem.payload.EmpDto;
 import com.example.Employeemanagementsystem.payload.EmpResponse;
 import com.example.Employeemanagementsystem.service.EmpService;
 import com.example.Employeemanagementsystem.utils.AppConstants;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,6 +19,9 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/employees")
+@Tag(
+        name = "CRUD rest api's for Employee resource"
+)
 public class EmpController {
     private EmpService service;
 
@@ -22,7 +29,14 @@ public class EmpController {
     super();
         this.service = service;
     }
-
+    @Operation(
+            summary = "Get all Employee rest Api",
+            description = "Get employee rest api is used to get all employee from  database"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "Http status 200 ok"
+    )
     @GetMapping
     public ResponseEntity<EmpResponse> getAllEmployees(
             @RequestParam(value = "pageNo",defaultValue = AppConstants.DEFAULT_PAGE_NUMBER,required = false) int pageNo,
@@ -32,25 +46,65 @@ public class EmpController {
         EmpResponse empDtoList = service.getAllEmployees(pageNo,pageSize,sortBy,sortDir);
         return ResponseEntity.ok(empDtoList);
     }
+    @Operation(
+            summary = "GET Employee by Id Api",
+            description = "GET Employee by Id Api is used to get employee by its id from database"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "Http status 200 ok"
+    )
     @GetMapping("/{id}")
     public ResponseEntity<EmpDto> getEmployeeById(@PathVariable("id") int id){
         EmpDto empDto= service.getEmployeeById(id);
         return ResponseEntity.ok(empDto);
     }
-    @PreAuthorize("hasRole('ADMIN')")
+    @Operation(
+            summary = "Create POST Employee rest Api",
+            description = "Create POST employee rest api is used to save employee in  database"
+    )
+    @ApiResponse(
+            responseCode = "201",
+            description = "Http status 201 created"
+    )
+    @SecurityRequirement(           //this annotation is to provide authorization header in rest api in swagger ui
+            name = "Bear Authentication"//here we have to pass security scheme name from securityconfig class
+    )
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     @PostMapping
     public ResponseEntity<EmpDto> createEmp(@Valid @RequestBody EmpDto empDto){
         EmpDto empResponse = service.createEmployee(empDto);
         return new ResponseEntity<EmpDto>(empResponse, HttpStatus.CREATED);
     }
-    //g
-    @PreAuthorize("hasRole('ADMIN')")
+    @Operation(
+            summary = "Update PUT Employee rest Api",
+            description = "Update PUT employee rest api is used to update a perticular employee in  database"
+    )
+    @ApiResponse(
+            responseCode = "201",
+            description = "Http status 201 created"
+    )
+    @SecurityRequirement(
+            name = "Bear Authentication"
+    )
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     @PutMapping("/{id}")
     public ResponseEntity<EmpDto> updateEmp(@Valid @RequestBody EmpDto empDto,@PathVariable("id") int newId){
         EmpDto empResponse = service.updateEmployee(empDto,newId);
         return ResponseEntity.ok(empResponse);
     }
-    @PreAuthorize("hasRole('ADMIN')")
+    @Operation(
+            summary = "Delete Employee rest Api",
+            description = "Delete employee rest api is used to delete employee from  database"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "Http status 200 ok"
+    )
+    @SecurityRequirement(
+            name = "Bear Authentication"
+    )
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     @DeleteMapping("/{id}")
     public ResponseEntity<String> deleteEmp(@PathVariable("id") int newId){
     service.deleteEmployee(newId);
--- src/main/java/com/example/Employeemanagementsystem/payload/EmpDto.java
+++ src/main/java/com/example/Employeemanagementsystem/payload/EmpDto.java
@@ -1,5 +1,6 @@
 package com.example.Employeemanagementsystem.payload;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
@@ -11,18 +12,32 @@ import java.util.Set;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Schema(
+        description = "EmpDto model Information"
+)
 public class EmpDto {
 
     private int id;
+    @Schema(
+            description = "Employee name"
+    )
     @NotEmpty
     @Size(min = 2,message = "Name should have atleast 2 characters")
     private String name;
+    @Schema(
+            description = "Employee title"
+    )
     @NotEmpty
     @Size(min = 2,message = "title should have atleast 2 characters")
     private String title;
+    @Schema(
+            description = "Employee project"
+    )
     @NotEmpty
     private String project;
     private Set<AddressDto> addressDtos;
-
+    @Schema(
+            description = "Employee's organization id"
+    )
     private int organizationId;
 }
